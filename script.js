8bbca7e367fec646840f45a97ee84c380c4b6cb1
Madina <madinanavrazova@gmail.com>
2025-02-12 00:37:23 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,45 @@
+const main = document.querySelector(".main");
+const colors = [
+  "#FBCEB1",
+  "#7FFFD4",
+  "#AB274F",
+  "#293133",
+  "#CCCCFF",
+  "#BDECB6",
+  "#1E5945",
+  "#F984E5",
+];
+
+function createBoxes(boxCount) {
+  main.innerHTML = "";
+  for (let i = 0; i < boxCount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.addEventListener("mouseenter", () => setColor(box));
+    main.appendChild(box);
+  }
+}
+
+function setColor(element) {
+  element.style.background = colors[Math.floor(Math.random() * colors.length)];
+}
+
+document.querySelector(".btn_size").addEventListener("click", () => {
+  let boxCount = parseInt(
+    prompt("Введите количество квадратов (максимум 100):"),
+    10
+  );
+  if (isNaN(boxCount) || boxCount < 1 || boxCount > 100) {
+    alert("Пожалуйста, введите число от 1 до 100.");
+  } else {
+    createBoxes(boxCount);
+  }
+});
+
+document.querySelector(".btn_clean").addEventListener("click", () => {
+  document
+    .querySelectorAll(".box")
+    .forEach((box) => (box.style.backgroundColor = "black"));
+});
+
+createBoxes(100);
